2605881f85af8c7ba7fc7b38577dc09f3ce21009
lsfernandes92 <lsfernandes92@gmail.com>
2019-05-28 17:44:06 -0300

    Add twitter service
    The purpose for this service is to only tweet to @paveoupacumer account
    on Twitter

---
+++ app/services/twitter_api/client_service.rb
@@ -0,0 +1,21 @@
+require 'twitter'
+
+module TwitterApi
+  class ClientService
+
+    def self.tweet(post)
+      client.update(post)
+    end
+
+      private
+
+      def self.client
+        Twitter::REST::Client.new do |config|
+          config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+          config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+          config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+          config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+        end
+      end
+  end
+end
---
+++ spec/services/twitter_api/client_service_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe TwitterApi::ClientService do
+  it 'tweets successsfully'
+  it 'fails to tweet'
+end
