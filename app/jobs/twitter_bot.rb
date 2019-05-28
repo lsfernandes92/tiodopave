44eb54740ebbdd5eed4ce411a578e7cb2bdcf695
lsfernandes92 <lsfernandes92@gmail.com>
2019-05-28 20:59:08 -0300

    Minor change to TwitterBot class

--- app/jobs/twitter_bot.rb
+++ app/jobs/twitter_bot.rb
@@ -1,17 +1,13 @@
 class TwitterBot
-  def initialize
-    @twitter_service = TwitterApi::PaveoupacumerService.new(post)
-  end
-
-  def post
-    RedditApi::TiodopaveService.new.get_newest_post[:title]
+  def initialize(post)
+    @post = post
   end
 
   def already_tweet?
-    @twitter_service.last_posted_tweet.include?(post)
+    TwitterApi::PaveoupacumerService.last_posted_tweet.include?(@post)
   end
 
   def fire_tweet
-    @twitter_service.tweet
+    TwitterApi::PaveoupacumerService.tweet(@post)
   end
 end
